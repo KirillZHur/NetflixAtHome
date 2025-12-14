1307429b4666ecd34250719a1ce7fcb8145fb566
YKudrin <xbhgbz@gmail.com>
2025-12-14 15:44:59 +0300

    add nx_metrics service

---
+++ nx_metrics/src/schema.py
@@ -0,0 +1,44 @@
+import time
+import jwt
+
+from flask import request
+from marshmallow import Schema, fields, pre_load
+
+
+class UserEventSchema(Schema):
+    user_event_tag = fields.Str()
+    user_id = fields.Str(allow_none=True, required=False)
+    event_time = fields.Str(required=False)
+
+    @pre_load
+    def extract_user_id(self, data: dict, **kwargs) -> dict:
+        token = request.headers.get("Authorization")
+        data["event_time"] = time.strftime("%Y-%m-%d %H:%M:%S")
+        if not token:
+            data["user_id"] = "2e991ac8-a0d5-46bf-973a-ee65199e06f0"
+            return data
+
+        decoded = jwt.decode(token, options={"verify_signature": False})
+        user_id = decoded.get('user_id')
+        data["user_id"] = user_id
+        return data
+
+
+class FilmEventSchema(Schema):
+    film_event_tag = fields.Str(allow_none=True)
+    film_id = fields.Str()
+    user_id = fields.Str(allow_none=True, required=False)
+    event_time = fields.Str(required=False)
+
+    @pre_load
+    def extract_user_id(self, data: dict, **kwargs) -> dict:
+        token = request.headers.get("Authorization")
+        data["event_time"] = time.strftime("%Y-%m-%d %H:%M:%S")
+        if not token:
+            data["user_id"] = "2e991ac8-a0d5-46bf-973a-ee65199e06f0"
+            return data
+
+        decoded = jwt.decode(token, options={"verify_signature": False})
+        user_id = decoded.get('user_id')
+        data["user_id"] = user_id
+        return data
